aacf404950b7494c15558fa6208b100d615d77af
webexninja <shivanktiware@gmail.com>
2016-03-04 01:05:52 +0530

    Add some more tests

--- spec/lib/printableDocument_spec.rb
+++ spec/lib/printableDocument_spec.rb
@@ -1,14 +1,15 @@
 require_relative "PrintableDocument.rb"
+require_relative 'spec_helper.rb'
 describe PrintableDocument do
-	let(:stub_printer) { double("printer") }
 
 	before :each do
 		@text = "A bunch of words"
 		@doc = PrintableDocument.new('test', 'nobody', @text, false)
 	end
 	it 'should know how to print itself' do
-		stub_printer.stub(:available?){true}
-		stub_printer.stub(:render){nil}
-		expect @doc.print(stub_printer) == 'Done'
+		mock_printer = mock('Printer')
+		mock_printer.should_receive(:available?).and_return(true)
+		mock_printer.should_receive(:render).exactly(3).times
+		@doc.print( mock_printer ).should == 'Done'
 	end
 end
